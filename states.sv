5f2815e45784ed01ea34c6b1e2589281d35f7f5b
harpreetsc1992 <hsc.10oct@gmail.com>
2019-02-11 03:07:58 -0500

    Just a compiled version; Needs interfacing with Fetch; needs testing

---
+++ states.sv
@@ -0,0 +1,2 @@
+typedef enum {INITIAL, WAIT_RESP, GOT_RESP} State;
+State state, next_state;
--- top.sv
+++ top.sv
@@ -1,4 +1,5 @@
 `include "Sysbus.defs"
+`include "states.sv"
 
 module top
 #(
@@ -27,15 +28,59 @@ module top
 
   logic [63:0] pc;
 
-  always @ (posedge clk)
+  always_comb begin
+	case(state)
+	  INITIAL	: begin
+	  	bus_req 	= pc;
+	  	bus_reqtag 	= {`SYSBUS_READ, `SYSBUS_MEMORY, 8'h00};
+	  	bus_reqcyc 	= 1;
+	  end
+	  WAIT_RESP	: bus_reqcyc 	= 0;
+	  GOT_RESP	: bus_respack 	= 1;
+	  default	: begin
+	  	bus_req   	= entry;
+      	bus_reqtag 	= {`SYSBUS_READ, `SYSBUS_MEMORY, 8'h00};
+      	bus_reqcyc 	= 0;
+	  	bus_reqcyc 	= 0;
+	  	bus_respack = 0;
+	  end
+	endcase
+  end
+  always_ff @ (posedge clk) begin
     if (reset) begin
       pc <= entry;
     end else begin
-      $display("Hello World!  @ %x", pc);
-      $finish;
+      //$display("Hello World!  @ %x", pc);
+	  if (bus_reqack == 1 		& next_state == WAIT_RESP & state == INITIAL) begin
+		state <= next_state;		
+	  end
+	  else if (bus_respcyc == 1 & next_state == GOT_RESP  & state == WAIT_RESP) begin
+		state <= next_state;
+	  end
+	  else if (bus_respcyc == 0 & next_state == INITIAL & state == GOT_RESP) begin
+		state <= next_state;
+	  end
+//      $finish;
     end
 
-  initial begin
-    $display("Initializing top, entry point = 0x%x", entry);
+//  initial begin
+//    $display("Initializing top, entry point = 0x%x", entry);
+  end
+
+  always_comb begin
+	case(state)
+	  INITIAL	: begin 
+		next_state = WAIT_RESP;
+	  end
+	  WAIT_RESP	: begin 
+		next_state = GOT_RESP;
+	  end
+	  GOT_RESP	: begin 
+		next_state = INITIAL;
+	  end
+	  default	: begin 
+		next_state = INITIAL;
+	  end
+	endcase
   end
 endmodule
