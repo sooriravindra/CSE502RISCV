94593eb413f10e6b89e0a63268b097e540a52ac5
Raveendra Soori <sooriravindra@gmail.com>
2019-02-12 17:20:56 -0500

    Add opcodes to decoder

--- decoder.sv
+++ decoder.sv
@@ -6,7 +6,19 @@ module decoder
  output [4:0] rd, //registers
  input [63:0] instr //input 32 bit instruction from PC
 );
- enum {opcodeR=7'b0110011, opcodeI=7'b0010011, opcodeS=7'b0100011, opcodeSB=7'b1100011, opcodeU=7'b0110111, opcodeUJ=7'b1101111} OPS;
+enum {
+    opcodeR1 = 7'b0110011, 
+    opcodeR2 = 7'b0111011, 
+    opcodeI1 = 7'b1100111, 
+    opcodeI2 = 7'b0000011,
+    opcodeI3 = 7'b0010011, 
+    opcodeI4 = 7'b0011011, 
+    opcodeS  = 7'b0100011, 
+    opcodeSB = 7'b1100011, 
+    opcodeU1 = 7'b0110111, 
+    opcodeU2 = 7'b0010111, 
+    opcodeUJ = 7'b1101111
+    } OPS;
  
  always_comb begin
    
