5cc17a42e9b98f9cec609b9222916274ae247ff3
Raveendra Soori <sooriravindra@gmail.com>
2019-02-12 16:36:29 -0500

    Added empty display functions

--- decoder.sv
+++ decoder.sv
@@ -4,7 +4,7 @@ module decoder
  output [4:0] rs1, 
  output [4:0] rs2, 
  output [4:0] rd, //registers
- input [32:0] instr //input 32 bit instruction from PC
+ input [63:0] instr //input 32 bit instruction from PC
 );
  enum {opcodeR=7'b0110011, opcodeI=7'b0010011, opcodeS=7'b0100011, opcodeSB=7'b1100011, opcodeU=7'b0110111, opcodeUJ=7'b1101111} OPS;
  
@@ -14,27 +14,34 @@ module decoder
       opcodeR: begin 
 	opcode = "R";
 	if (instr[14:12] == 3'b000) begin
-		
+	    $display("");	
 	
 	end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
 	end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
 	end
@@ -44,27 +51,35 @@ module decoder
     opcodeI: begin
 	opcode = "I";
 	if (instr[14:12] == 3'b000) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
         end
@@ -73,27 +88,35 @@ module decoder
     opcodeS: begin
 	opcode = "S";
 	if (instr[14:12] == 3'b000) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
         end
@@ -102,27 +125,35 @@ module decoder
     opcodeSB: begin
 	opcode = "SB";
 	if (instr[14:12] == 3'b000) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
         end
@@ -131,27 +162,35 @@ module decoder
     opcodeU: begin
 	opcode = "U";
 	if (instr[14:12] == 3'b000) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
         end
@@ -160,27 +199,35 @@ module decoder
     opcodeUJ: begin
 	opcode = "UJ";
 	if (instr[14:12] == 3'b000) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b001) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b010) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b011) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b100) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b101) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b110) begin
+	    $display("");	
 
 
         end else if (instr[14:12] == 3'b111) begin
+	    $display("");	
 
 
         end
